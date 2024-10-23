5c136877e1bfac35710fbc711f16d4fabea07f21
rn <admin@rsquad.io>
2024-10-23 19:17:10 +0700

    lib: change confirmations count

--- lib/pkg/bitcoin/client.go
+++ lib/pkg/bitcoin/client.go
@@ -1,91 +1,91 @@
 package bitcoin
 
 import (
-    "encoding/json"
-    "os"
+	"encoding/json"
+	"os"
 
-    "github.com/btcsuite/btcd/btcjson"
-    "github.com/btcsuite/btcd/chaincfg/chainhash"
-    "github.com/btcsuite/btcd/rpcclient"
+	"github.com/btcsuite/btcd/btcjson"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/btcsuite/btcd/rpcclient"
 )
 
-const ConfirmationsNeeded = 3
+const ConfirmationsNeeded = 6
 
 type Client struct {
-    RPCClient *rpcclient.Client
+	RPCClient *rpcclient.Client
 }
 
 func NewClient() (*Client, error) {
-    connCfg := &rpcclient.ConnConfig{
-        Host:         os.Getenv("COMMON_BITCOIN_RPC_HOST"),
-        User:         os.Getenv("COMMON_BITCOIN_RPC_USER"),
-        Pass:         os.Getenv("COMMON_BITCOIN_RPC_PASS"),
-        HTTPPostMode: true,
-        DisableTLS:   true,
-    }
+	connCfg := &rpcclient.ConnConfig{
+		Host:         os.Getenv("COMMON_BITCOIN_RPC_HOST"),
+		User:         os.Getenv("COMMON_BITCOIN_RPC_USER"),
+		Pass:         os.Getenv("COMMON_BITCOIN_RPC_PASS"),
+		HTTPPostMode: true,
+		DisableTLS:   true,
+	}
 
-    rpcClient, err := rpcclient.New(connCfg, nil)
-    if err != nil {
-        return nil, err
-    }
+	rpcClient, err := rpcclient.New(connCfg, nil)
+	if err != nil {
+		return nil, err
+	}
 
-    return &Client{
-        RPCClient: rpcClient,
-    }, nil
+	return &Client{
+		RPCClient: rpcClient,
+	}, nil
 }
 
 func (c *Client) GetBlockHeightByHash(hash *chainhash.Hash) (int64, error) {
-    blockVerbose, err := c.RPCClient.GetBlockVerbose(hash)
-    if err != nil {
-        return 0, err
-    }
-    return blockVerbose.Height, nil
+	blockVerbose, err := c.RPCClient.GetBlockVerbose(hash)
+	if err != nil {
+		return 0, err
+	}
+	return blockVerbose.Height, nil
 }
 
 func (c *Client) GetBlockHashesByStartHeight(startHeight int64, count int64) ([]*chainhash.Hash, error) {
-    var loopErr error
-    blockHashes := make([]*chainhash.Hash, 0, count)
-    for i := int64(0); i < count; i++ {
-        blockHash, err := c.RPCClient.GetBlockHash(startHeight + i)
-        if err != nil {
-            loopErr = err
-            break
-        }
-        blockHashes = append(blockHashes, blockHash)
-    }
-    return blockHashes, loopErr
+	var loopErr error
+	blockHashes := make([]*chainhash.Hash, 0, count)
+	for i := int64(0); i < count; i++ {
+		blockHash, err := c.RPCClient.GetBlockHash(startHeight + i)
+		if err != nil {
+			loopErr = err
+			break
+		}
+		blockHashes = append(blockHashes, blockHash)
+	}
+	return blockHashes, loopErr
 }
 
 func (c *Client) GetBlockHashByTxID(txID *chainhash.Hash) (*chainhash.Hash, error) {
-    tx, err := c.RPCClient.GetRawTransactionVerbose(txID)
-    if err != nil {
-        return nil, err
-    }
-    return chainhash.NewHashFromStr(tx.BlockHash)
+	tx, err := c.RPCClient.GetRawTransactionVerbose(txID)
+	if err != nil {
+		return nil, err
+	}
+	return chainhash.NewHashFromStr(tx.BlockHash)
 }
 
 func (c *Client) GetTxProof(txID *chainhash.Hash, blockHash *chainhash.Hash) (string, error) {
-    blockHashStr := blockHash.String()
-    blockHashStrPtr := &blockHashStr
-    cmd := btcjson.NewGetTxOutProofCmd([]string{txID.String()}, blockHashStrPtr)
+	blockHashStr := blockHash.String()
+	blockHashStrPtr := &blockHashStr
+	cmd := btcjson.NewGetTxOutProofCmd([]string{txID.String()}, blockHashStrPtr)
 
-    responseChannel := c.RPCClient.SendCmd(cmd)
+	responseChannel := c.RPCClient.SendCmd(cmd)
 
-    response, err := rpcclient.ReceiveFuture(responseChannel)
-    if err != nil {
-        return "", err
-    }
+	response, err := rpcclient.ReceiveFuture(responseChannel)
+	if err != nil {
+		return "", err
+	}
 
-    var proof string
+	var proof string
 
-    err = json.Unmarshal(response, &proof)
-    if err != nil {
-        return "", err
-    }
+	err = json.Unmarshal(response, &proof)
+	if err != nil {
+		return "", err
+	}
 
-    return proof, nil
+	return proof, nil
 }
 
 func (c *Client) ShutdownRPCClient() {
-    c.RPCClient.Shutdown()
+	c.RPCClient.Shutdown()
 }
