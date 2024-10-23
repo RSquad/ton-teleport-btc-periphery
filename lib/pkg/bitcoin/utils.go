aecb41441335050d67c4bcd93978655db745f5da
rn <admin@rsquad.io>
2024-10-23 19:14:10 +0700

    common: reformat and cleanup repo

--- lib/pkg/bitcoin/utils.go
+++ lib/pkg/bitcoin/utils.go
@@ -1,23 +1,23 @@
 package bitcoin
 
 import (
-	"bytes"
+    "bytes"
 
-	"github.com/btcsuite/btcd/chaincfg/chainhash"
-	"github.com/btcsuite/btcd/wire"
+    "github.com/btcsuite/btcd/chaincfg/chainhash"
+    "github.com/btcsuite/btcd/wire"
 )
 
 func SliceOfHashesContains(haystack []*chainhash.Hash, needle *chainhash.Hash) bool {
-	for _, item := range haystack {
-		if item.IsEqual(needle) {
-			return true
-		}
-	}
-	return false
+    for _, item := range haystack {
+        if item.IsEqual(needle) {
+            return true
+        }
+    }
+    return false
 }
 
 func BlockHeaderToBytes(header *wire.BlockHeader) ([]byte, error) {
-	var buf bytes.Buffer
-	err := header.Serialize(&buf)
-	return buf.Bytes(), err
+    var buf bytes.Buffer
+    err := header.Serialize(&buf)
+    return buf.Bytes(), err
 }
--- lib/pkg/ton/client.go
+++ lib/pkg/ton/client.go
@@ -1,33 +1,33 @@
 package ton
 
 import (
-	"context"
-	"os"
+    "context"
+    "os"
 
-	"github.com/xssnick/tonutils-go/liteclient"
-	"github.com/xssnick/tonutils-go/ton"
+    "github.com/xssnick/tonutils-go/liteclient"
+    "github.com/xssnick/tonutils-go/ton"
 )
 
 type Client struct {
-	Pool *liteclient.ConnectionPool
-	API  *ton.APIClient
+    Pool *liteclient.ConnectionPool
+    API  *ton.APIClient
 }
 
 func NewClient() (*Client, error) {
-	pool := liteclient.NewConnectionPool()
+    pool := liteclient.NewConnectionPool()
 
-	configURL := os.Getenv("COMMON_TON_CONFIG_URL")
+    configURL := os.Getenv("COMMON_TON_CONFIG_URL")
 
-	err := pool.AddConnectionsFromConfigUrl(context.Background(), configURL)
-	if err != nil {
-		return nil, err
-	}
+    err := pool.AddConnectionsFromConfigUrl(context.Background(), configURL)
+    if err != nil {
+        return nil, err
+    }
 
-	api := ton.NewAPIClient(pool)
+    api := ton.NewAPIClient(pool)
 
-	return &Client{
-		Pool: pool,
-		API:  api,
-	}, nil
+    return &Client{
+        Pool: pool,
+        API:  api,
+    }, nil
 
 }
