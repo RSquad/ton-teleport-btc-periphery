2bb47063e89df672231234b5983bf917968d635f
Aleksandr Volkov <avolkov05@gmail.com>
2024-12-11 13:05:47 +0700

    add validator console

---
+++ lib/pkg/ton/validator_console_engine/validator_console_engine.go
@@ -0,0 +1,153 @@
+package validator_console_engine
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os/exec"
+	"regexp"
+	"strings"
+)
+
+const DefaultVerbosityLevel = 0
+
+type Validator struct {
+	ID           string `json:"id"`
+	ElectionDate int    `json:"election_date"`
+	ExpireAt     int    `json:"expire_at"`
+}
+
+type ValidatorEngineConfig struct {
+	Validators []Validator `json:"validators"`
+}
+
+type BaseCommandParams struct {
+	P string // Server public key path
+	K string // Client private key path
+	A string // Server address
+	V int    // Verbosity level
+}
+
+type ValidatorEngineCommand struct {
+	BaseCommandParams
+	C string
+}
+
+type ValidatorEngineConsole struct {
+	ValidatorEngineConsolePath string
+	ServerPublicKeyPath        string
+	ClientPrivateKeyPath       string
+	ServerAddress              string
+	baseCommandParams          BaseCommandParams
+}
+
+func NewValidatorEngineConsole(
+	validatorEngineConsolePath string,
+	serverPublicKeyPath string,
+	clientPrivateKeyPath string,
+	serverAddress string,
+) *ValidatorEngineConsole {
+	baseCommandParams := BaseCommandParams{
+		P: serverPublicKeyPath,
+		K: clientPrivateKeyPath,
+		A: serverAddress,
+		V: DefaultVerbosityLevel,
+	}
+	return &ValidatorEngineConsole{
+		ValidatorEngineConsolePath: validatorEngineConsolePath,
+		ServerPublicKeyPath:        serverPublicKeyPath,
+		ClientPrivateKeyPath:       clientPrivateKeyPath,
+		ServerAddress:              serverAddress,
+		baseCommandParams:          baseCommandParams,
+	}
+}
+
+func (v *ValidatorEngineConsole) GetValidatorConfig() *ValidatorEngineConfig {
+	params := ValidatorEngineCommand{
+		BaseCommandParams: v.baseCommandParams,
+		C:                 "getconfig",
+	}
+
+	result, err := v.runCommand(params)
+	if err != nil {
+		log.Printf("Error running command getconfig: %v", err)
+	}
+
+	config, _ := v.parseGetValidatorConfigOutput(result)
+	log.Printf("%v", config)
+
+	return config
+}
+
+func (v *ValidatorEngineConsole) ExportPub(validatorIdBase64 string) string {
+	command := fmt.Sprintf("exportpub %x", validatorIdBase64)
+
+	params := ValidatorEngineCommand{
+		BaseCommandParams: v.baseCommandParams,
+		C:                 command,
+	}
+	result, err := v.runCommand(params)
+	if err != nil {
+		log.Printf("Error running command exportpub: %v", err)
+	}
+
+	publicKey, _ := v.parseExportPubOutput(result)
+
+	return publicKey
+}
+
+func (v *ValidatorEngineConsole) runCommand(params ValidatorEngineCommand) (string, error) {
+	executable := fmt.Sprintf("%s/validator-engine-console", v.ValidatorEngineConsolePath)
+	command := v.buildCommand(executable, params)
+	log.Printf("Executing command: %s", command)
+	result, err := v.execute(command)
+	if err != nil {
+		log.Fatalf("Error running command: %v", err)
+	}
+	return result, nil
+}
+
+func (v *ValidatorEngineConsole) buildCommand(executable string, parameters ValidatorEngineCommand) string {
+	return fmt.Sprintf("%s -p %s -k %s -a %s -v %d -c \"%s\"",
+		executable,
+		parameters.P,
+		parameters.K,
+		parameters.A,
+		parameters.V,
+		parameters.C,
+	)
+}
+
+func (v *ValidatorEngineConsole) execute(command string) (string, error) {
+	stdout, err := exec.Command("bash", "-c", command).Output()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	log.Printf("Stdout: %s", string(stdout))
+
+	return string(stdout), nil
+}
+
+func (v *ValidatorEngineConsole) parseGetValidatorConfigOutput(output string) (*ValidatorEngineConfig, error) {
+	re := regexp.MustCompile(`(?s)-+\s*\n(.*?)\n-+`)
+	match := re.FindStringSubmatch(output)
+	if len(match) < 2 {
+		return nil, fmt.Errorf("Error parsing JSON from output")
+	}
+	jsonStr := strings.TrimSpace(match[1])
+	var config ValidatorEngineConfig
+	err := json.Unmarshal([]byte(jsonStr), &config)
+	if err != nil {
+		return nil, fmt.Errorf("Error unmarshaling JSON: %v", err)
+	}
+	return &config, nil
+}
+
+func (v *ValidatorEngineConsole) parseExportPubOutput(output string) (string, error) {
+	re := regexp.MustCompile(`got public key:\s*([A-Za-z0-9+/=]+)`)
+	match := re.FindStringSubmatch(output)
+	if len(match) < 2 {
+		return "", fmt.Errorf("Error parsing public key from output")
+	}
+	return match[1], nil
+}
--- oracle/cmd/main.go
+++ oracle/cmd/main.go
@@ -6,11 +6,14 @@ import (
 	"log"
 	"strings"
 
+	"github.com/rsquad/ton-teleport-btc-periphery/lib/pkg/ton/validator_console_engine"
+
 	jwv4r2contract "github.com/rsquad/ton-teleport-btc-periphery/lib/pkg/ton/jw_v4r2_contract"
 	"github.com/xssnick/tonutils-go/address"
 	"github.com/xssnick/tonutils-go/ton/wallet"
 
-	coordinatorcontract "github.com/rsquad/ton-teleport-btc-periphery/lib/pkg/ton/coordinatorcontract"
+	"github.com/rsquad/ton-teleport-btc-periphery/lib/pkg/ton/coordinatorcontract"
+
 	tonclient "github.com/rsquad/ton-teleport-btc-periphery/lib/pkg/ton/ton_client"
 	"github.com/rsquad/ton-teleport-btc-periphery/lib/pkg/utils"
 	"github.com/rsquad/ton-teleport-btc-periphery/oracle/internal/config"
@@ -66,14 +69,27 @@ func initialize() (*App, error) {
 }
 
 func main() {
-	app, err := initialize()
-	if err != nil {
-		log.Fatalf("[App] failed to initialize: %v", err)
-	}
+	//app, err := initialize()
+	//if err != nil {
+	//	log.Fatalf("[App] failed to initialize: %v", err)
+	//}
 
-	prevDkg, err := app.CoordinatorContract.GetPrevDKG()
-	if err != nil {
-		log.Fatalf("[App] Get prev dkg error: %v", err)
-	}
-	log.Printf("PrevDKG = %v", prevDkg)
+	//dkg, err := app.CoordinatorContract.GetDKG()
+	//if err != nil {
+	//	log.Fatalf("[App] Get dkg  error: %v", err)
+	//}
+	//log.Printf("fDKG = %v", dkg)
+	//prevDkg, err := app.CoordinatorContract.GetPrevDKG()
+	//if err != nil {
+	//	log.Fatalf("[App] Get prev dkg error: %v", err)
+	//}
+	//log.Printf("PrevDKG = %v", prevDkg)
+
+	validatorConsoleEngine := validator_console_engine.NewValidatorEngineConsole(
+		"/Users/cryptovolk/www/rsquad/myLocalTon/genesis/bin",
+		"/Users/cryptovolk/www/rsquad/myLocalTon/genesis/bin/certs/server.pub",
+		"/Users/cryptovolk/www/rsquad/myLocalTon/genesis/bin/certs/client",
+		"127.0.0.1:4441")
+
+	validatorConsoleEngine.ExportPub("EZdGkyd0hS7bmxXMxkxztBTrsKr/BWBO3JNnkyWejs0=")
 }
