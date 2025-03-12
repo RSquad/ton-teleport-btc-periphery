ce11db4700736ce0c3989be16222f4f0a22637cb
Anton Varlamov <anton.varlamov@rsquad.io>
2025-03-12 15:16:31 +0300

    WIP: add validator console

--- oracle/internal/validator/validator.go
+++ oracle/internal/validator/validator.go
@@ -8,6 +8,7 @@ import (
 	"github.com/rsquad/ton-teleport-btc-periphery/lib/pkg/ton/coordinator"
 	"github.com/rsquad/ton-teleport-btc-periphery/lib/pkg/ton/signer"
 	"github.com/rsquad/ton-teleport-btc-periphery/oracle/internal/cfg"
+	validatorconsole "github.com/rsquad/ton-teleport-btc-periphery/oracle/internal/validator_console"
 )
 
 type ValidatorKeyInfo struct {
@@ -20,12 +21,14 @@ type Validator struct {
 	standaloneMode       bool
 	standalonePrivateKey []byte
 	standalonePublicKey  []byte
+	validatorConsole     validatorconsole.ValidatorConsole
 }
 
 func NewValidator(cfg *cfg.Cfg) (*Validator, error) {
 	var standalonePublicKey []byte = nil
 	var standalonePrivateKey []byte = nil
 	var err error = nil
+	var console validatorconsole.ValidatorConsole
 	if cfg.StandaloneMode {
 		standalonePublicKey, err = hex.DecodeString(cfg.Pubkey)
 		if err != nil {
@@ -35,11 +38,19 @@ func NewValidator(cfg *cfg.Cfg) (*Validator, error) {
 		if err != nil {
 			return nil, err
 		}
+	} else {
+		console = *validatorconsole.NewValidatorConsole(
+			cfg.ValidatorEngineConsolePath,
+			cfg.ServerPublicKeyPath,
+			cfg.ClientPrivateKeyPath,
+			cfg.ValidatorServerAddr,
+		)
 	}
 	return &Validator{
 		standaloneMode:       cfg.StandaloneMode,
 		standalonePublicKey:  standalonePublicKey,
 		standalonePrivateKey: standalonePrivateKey,
+		validatorConsole:     console,
 	}, nil
 }
 
@@ -56,13 +67,32 @@ func (v *Validator) FindKeyInfo(vset coordinator.VSet) (*ValidatorKeyInfo, error
 			}
 		}
 		return nil, errors.New("validator key not found")
+	} else {
+		vals, err := v.validatorConsole.GetValidatorKeys()
+		if err != nil {
+			return nil, err
+		}
+		for _, val := range vals {
+			for idx, pubkey := range vset {
+				if bytes.Equal(pubkey, val.ValidatorKey) {
+					return &ValidatorKeyInfo{
+						KeyID:     []byte(val.ValidatorId),
+						VsetIdx:   idx,
+						PublicKey: val.ValidatorKey,
+					}, nil
+				}
+			}
+		}
 	}
-	panic("not implemented")
+	return nil, errors.New("validator key not found")
 }
 
 func (v *Validator) GetSigner(keyID []byte) *signer.Signer {
 	if v.standaloneMode {
 		return signer.New(hex.EncodeToString(v.standalonePrivateKey))
 	}
+	// else {
+	// 	return v.validatorConsole.NewValidatorSigner(keyID)
+	// }
 	panic("not implemented")
 }
---
+++ oracle/internal/validator_console/service.go
@@ -0,0 +1,209 @@
+package validatorconsole
+
+import (
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"os/exec"
+	"regexp"
+	"strings"
+
+	"github.com/xssnick/tonutils-go/tvm/cell"
+)
+
+type ValidatorTempKey struct {
+	Key      string `json:"key"`
+	ExpireAt int64  `json:"expire_at"`
+}
+
+type ValidatorAdnlAddress struct {
+	Id       string `json:"id"`
+	ExpireAt int64  `json:"expire_at"`
+}
+
+type Validator struct {
+	Id           string                 `json:"id"`
+	TempKeys     []ValidatorTempKey     `json:"temp_keys"`
+	AdnlAddrs    []ValidatorAdnlAddress `json:"adnl_addrs"`
+	ElectionDate int64                  `json:"election_date"`
+	ExpireAt     int64                  `json:"expire_at"`
+}
+
+type ValidatorEngineConfig struct {
+	OutPort    int64
+	Validators []Validator
+}
+
+type ValidatorConsole struct {
+	validatorEngineConsolePath string
+	serverPublicKeyPath        string
+	clientPrivateKeyPath       string
+	serverAddress              string
+}
+
+type ValidatorConsoleKey struct {
+	ValidatorKey []byte
+	ValidatorId  string
+}
+
+func NewValidatorConsole(
+	validatorEngineConsolePath string,
+	serverPublicKeyPath string,
+	clientPrivateKeyPath string,
+	serverAddress string,
+) *ValidatorConsole {
+	return &ValidatorConsole{
+		validatorEngineConsolePath: validatorEngineConsolePath,
+		serverPublicKeyPath:        serverPublicKeyPath,
+		clientPrivateKeyPath:       clientPrivateKeyPath,
+		serverAddress:              serverAddress,
+	}
+}
+
+func (v *ValidatorConsole) runCommand(c string) ([]byte, error) {
+	args := v.buildCommand(c)
+
+	res := exec.Command(v.validatorEngineConsolePath+"/validator-engine-console", args...)
+
+	out, err := res.CombinedOutput()
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println(string(out))
+		return nil, err
+	}
+
+	return out, nil
+}
+func (v *ValidatorConsole) buildCommand(c string) []string {
+	command := []string{}
+	command = append(command, "-c", c)
+	command = append(command, "-p", v.serverPublicKeyPath)
+	command = append(command, "-k", v.clientPrivateKeyPath)
+	command = append(command, "-a", v.serverAddress)
+	command = append(command, "-v", "\"0\"")
+
+	res := command
+	return res
+}
+
+func (v *ValidatorConsole) GetValidatorKeys() ([]ValidatorConsoleKey, error) {
+
+	command, err := v.runCommand("getconfig")
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	fixedStr, err := extractJSON((string(command)))
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	var obj ValidatorEngineConfig
+	err1 := json.Unmarshal([]byte(fixedStr), &obj)
+	if err1 != nil {
+		fmt.Println(err1)
+		return nil, err1
+	}
+
+	fmt.Println("GetValidatorKeys")
+	validatorConsoleKeys := []ValidatorConsoleKey{}
+	for _, validator := range obj.Validators {
+		fmt.Println(validator.Id)
+		pubKey, err := v.exportPub(validator.Id)
+		if err != nil {
+			return nil, err
+		}
+		fmt.Println("GetValidatorKeys")
+		base64Id, err := base64.StdEncoding.DecodeString(validator.Id)
+
+		if err != nil {
+			return nil, err
+		}
+		fmt.Println("GetValidatorKeys")
+		validatorId := hex.EncodeToString(base64Id)
+
+		validatorConsoleKey := ValidatorConsoleKey{
+			ValidatorKey: pubKey,
+			ValidatorId:  validatorId,
+		}
+		validatorConsoleKeys = append(validatorConsoleKeys, validatorConsoleKey)
+	}
+	fmt.Println("GetValidatorKeys END")
+	return validatorConsoleKeys, nil
+}
+
+func extractJSON(input string) (string, error) {
+	re := regexp.MustCompile(`-+[\r\n]+([\s\S]*?)[\r\n]+-+`)
+	match := re.FindString(input)
+	if match == "" {
+		return "", fmt.Errorf("no JSON found in input")
+	}
+	return strings.Trim(strings.TrimSpace(match), "-"), nil
+}
+
+func extractPublicKey(output string) string {
+	regex := regexp.MustCompile(`got public key:\s*([A-Za-z0-9+/=]+)`)
+	match := regex.FindString(output)
+	match = strings.Trim(match, "got public key: ")
+	return match
+}
+
+func (v *ValidatorConsole) exportPub(validatorIdBase64 string) ([]byte, error) {
+	p, err := base64.StdEncoding.DecodeString(validatorIdBase64)
+	if err != nil {
+		return nil, err
+	}
+
+	validatorKey := hex.EncodeToString(p)
+	result, err := v.runCommand(fmt.Sprintf("exportpub %s", validatorKey))
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(string(result))
+	base64Result := extractPublicKey(string(result))
+	fmt.Println(base64Result)
+	d, err := base64.StdEncoding.DecodeString(base64Result)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return d, nil
+}
+
+func extractSignature(output string) string {
+	regex := regexp.MustCompile(`got signature\s+([A-Za-z0-9+/=]+)`)
+	match := regex.FindString(output)
+	match = strings.Trim(match, "got signature: ")
+	return match
+}
+
+func (v *ValidatorConsole) Sign(validatorId string, hash string) (string, error) {
+	result, err := v.runCommand(fmt.Sprintf("sign %s %s", validatorId, hash))
+	if err != nil {
+		return "", err
+	}
+	return extractSignature(string(result)), nil
+}
+
+type ValidatorSigner struct {
+	validatorEngineCondole ValidatorConsole
+	publicKey              string
+}
+
+func (v *ValidatorConsole) NewValidatorSigner(publicKey string) *ValidatorSigner {
+	return &ValidatorSigner{validatorEngineCondole: *v, publicKey: publicKey}
+}
+
+func (s *ValidatorSigner) SignCell(cell *cell.Cell) []byte {
+	cellHex := hex.EncodeToString(cell.Hash())
+	result, err := s.validatorEngineCondole.Sign(s.publicKey, cellHex)
+	if err != nil {
+		return nil
+	}
+
+	return []byte(result)
+}
