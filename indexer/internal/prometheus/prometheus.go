b93e6ca03b100753e45d163ae11f7595e951b6e0
Artem <sleeper979@gmail.com>
2025-03-14 17:55:06 +0300

    add contracts balances metric

--- indexer/internal/prometheus/prometheus.go
+++ indexer/internal/prometheus/prometheus.go
@@ -24,45 +24,48 @@ func New(tonClient *tonclient.TonClient, config config.IndexerConfig) *Prometheu
 	}
 }
 
-func recordMetrics(balance int64) {
+func recordMetrics(balances map[string]float64) {
 	go func() {
-			for {
-					contractBalances.Set(float64(balance))
+			for key, value := range balances {
+					contractBalances.With(prometheus.Labels{"contract_name": key}).Set(value)
 			}
 	}()
 }
 
 var (
-	contractBalances = promauto.NewGauge(prometheus.GaugeOpts{
+	contractBalances = promauto.NewGaugeVec(prometheus.GaugeOpts{
 			Name: "teleport_contracts_balances",
-			Help: "Current contract balances",
-	})
+			Help: "Current contracts balances",
+	}, []string{"contract_name"})
 )
 
 func (p *Prometheus) Work(ctx context.Context) (err error) {
 	v := reflect.ValueOf(p.config)
+	typeOfC := v.Type()
+	balances := make(map[string]float64)
 
 	for i := range v.NumField() {
 		field := v.Field(i)
 		var fieldValue string = field.Interface().(string)
+		fieldName := typeOfC.Field(i).Name
 		contractAddress, err := address.ParseAddr(fieldValue)
-
 		if err != nil {
 			continue
 		}
 		
 		balance, err := p.getContractBalance(ctx, contractAddress)
-		recordMetrics(balance)
 		if err != nil {
-			return fmt.Errorf("failed to record metrics: %v", err)
+			return fmt.Errorf("failed to get contract balance: %v", err)
 		}
+		balances[fieldName] = float64(balance) / 1000000000
+		fmt.Println("balances: ", balances)
+		recordMetrics(balances)
 	
 	}
 	return nil
 }
 
 func (p *Prometheus) getContractBalance(ctx context.Context, addr *address.Address) (int64, error) {
-	// Get the account state
 	block, err := p.tonClient.API.CurrentMasterchainInfo(ctx)
 
 	if err != nil {
@@ -74,6 +77,5 @@ func (p *Prometheus) getContractBalance(ctx context.Context, addr *address.Addre
 		return 0, fmt.Errorf("failed to get account: %v", err)
 	}
 
-	// Return the balance in nanoTON
 	return account.State.Balance.Nano().Int64(), nil
 }
