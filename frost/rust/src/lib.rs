da94c29a89ed32a03d62d3d77b83da2497af754d
NikitaM <21960067+Keshoid@users.noreply.github.com>
2025-03-28 14:40:39 +0300

    fmt(frost): format code

--- frost/rust/src/lib.rs
+++ frost/rust/src/lib.rs
@@ -63,8 +63,10 @@ macro_rules! from_bytes_for {
                 unsafe {
                     let packages = slice::from_raw_parts(ptr, len);
                     for p in packages {
-                        let identifier = Identifier::deserialize(&p.identifier).expect("Cannot deserialize Identifier");
-                        let pkg = $T::from_raw_parts(p.buf, p.len).expect("Cannot deserialize Package");
+                        let identifier = Identifier::deserialize(&p.identifier)
+                            .expect("Cannot deserialize Identifier");
+                        let pkg =
+                            $T::from_raw_parts(p.buf, p.len).expect("Cannot deserialize Package");
                         map.insert(identifier, pkg);
                     }
                 }
@@ -201,11 +203,7 @@ pub extern "C" fn dkg_part3(
     let r2_secret_box = from_void(r2_secret);
     let r1_pkgs_map = Round1Package::make_map(r1_pkgs_ptr, r1_pkgs_len);
     let r2_pkgs_map = Round2Package::make_map(r2_pkgs_ptr, r2_pkgs_len);
-    let result = frost_dkg_part3(
-        &r2_secret_box,
-        &r1_pkgs_map,
-        &r2_pkgs_map,
-    );
+    let result = frost_dkg_part3(&r2_secret_box, &r1_pkgs_map, &r2_pkgs_map);
     // Prevent r2_secret_box from being freed. It must be freed manually.
     Box::leak(r2_secret_box);
     match result {
@@ -231,9 +229,9 @@ pub extern "C" fn dkg_part3(
 
 #[no_mangle]
 pub extern "C" fn free_r2_secret(r2_secret: *mut c_void) {
-    unsafe { 
+    unsafe {
         if !r2_secret.is_null() {
-            let _ =Box::from_raw(r2_secret as *mut Round2SecretPackage);
+            let _ = Box::from_raw(r2_secret as *mut Round2SecretPackage);
         }
     };
 }
@@ -345,8 +343,7 @@ pub extern "C" fn aggregate_with_tweak(
         message_buf.to_slice(),
     );
 
-    let signature_shares_map =
-        SignatureShare::make_map(signature_shares_ptr, signature_shares_len);
+    let signature_shares_map = SignatureShare::make_map(signature_shares_ptr, signature_shares_len);
 
     let result = frost_aggregate_with_tweak(
         &signing_package,
@@ -411,12 +408,12 @@ pub extern "C" fn extract_public_key_from_package(
         Ok(x) => x,
         Err(_) => return -1,
     };
-    
+
     let key_vec = match pubkey_package.verifying_key().serialize() {
         Ok(x) => x,
         Err(_) => return -2,
     };
-    
+
     unsafe {
         (*public_key).len = key_vec.len();
         (*public_key).data = key_vec.leak().as_ptr();
