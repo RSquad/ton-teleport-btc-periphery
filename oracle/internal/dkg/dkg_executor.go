d9a9f35a8d5311cb7481b4ef44d28c4b02ecc70c
it_bear <big.black.fox@gmail.com>
2025-06-06 04:12:06 +0400

    Fixed the deserialization of the DkgR2 package (TOB-TGBTC-17) (#155)
    * Fixed the deserialization of the DkgR2 package (TOB-TGBTC-17)
    
    * Refactored after code review
    
    * Fix bug with non-deterministic iteration over map
    
    * Debug
    
    * Fixed test
    
    * remove unused code
    
    ---------
    
    Co-authored-by: it_bear <dmitry.varlamov@rsquad.io>
    Co-authored-by: NikitaM <21960067+Keshoid@users.noreply.github.com>

--- oracle/internal/dkg/dkg_executor.go
+++ oracle/internal/dkg/dkg_executor.go
@@ -370,7 +370,6 @@ func (e *Executor) executeR3(dkg *coordinator.DKG) bool {
 		r2Packages, isCulpritFound, culpritIdx, err := helpers.DeserializeDkgR2(
 			dkg.GetR2Packages(),
 			dkg.VSetMask,
-			dkg.MaxSigners,
 		)
 
 		if isCulpritFound {
