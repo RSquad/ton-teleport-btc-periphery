9965156106bcf4dd4669cf71cbf1fb072b337267
Artem <sleeper979@gmail.com>
2025-08-01 18:38:13 +0300

    add utxo, fee metrics

--- indexer/internal/metrics/metrics_variables.go
+++ indexer/internal/metrics/metrics_variables.go
@@ -41,4 +41,35 @@ var (
 		},
 		[]string{"pegout_addr", "bitcoin_tx_id"},
 	)
+	utxoLimitGauge = promauto.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "utxo_limit",
+			Help: "UTXO limit",
+		},
+	)
+	utxoCountGauge = promauto.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "utxo_count",
+			Help: "UTXO count",
+		},
+	)
+	totalSetrviceFeeGauge = promauto.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "total_service_fee",
+			Help: "Total service fee",
+		},
+	)
+	autopegoutFeeGauge = promauto.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "autopegout_fee",
+			Help: "Autopegout fee",
+		},
+	)
+	relayerBalance = promauto.NewGaugeVec( // TODO: implement metric
+		prometheus.GaugeOpts{
+			Name: "relayer_balance",
+			Help: "Relayer balance",
+		},
+		[]string{"addr"},
+	)
 )
