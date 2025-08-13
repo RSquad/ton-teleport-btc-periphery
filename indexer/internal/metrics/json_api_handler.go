33fdd20581c0f22fe45e1afac9534c5ca47fa6b0
it_bear <big.black.fox@gmail.com>
2025-08-13 12:02:34 +0400

    Feat/ttbtc 770 (#253)
    * Remove DKG packages from the database store and add a new API endpoint for validator information

--- indexer/internal/metrics/json_api_handler.go
+++ indexer/internal/metrics/json_api_handler.go
@@ -1,20 +1,33 @@
 package metrics
 
 import (
+	"context"
 	"database/sql"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"math/big"
 	"net/http"
+	"strconv"
+	"strings"
 	"time"
+
+	"github.com/rsquad/ton-teleport-btc-periphery/lib/pkg/ton/tonclient"
 )
 
 type JsonApiHandler struct {
-	db    *sql.DB
-	cache *Cache[string]
+	db                   *sql.DB
+	tonClient            *tonclient.TonClient
+	tonMaxMainValidators int
+	cache                *Cache[string]
 }
 
-func NewJsonApiHandler(db *sql.DB) *JsonApiHandler {
+func NewJsonApiHandler(db *sql.DB, tonClient *tonclient.TonClient) *JsonApiHandler {
 	return &JsonApiHandler{
-		db:    db,
-		cache: NewCache[string](),
+		db:                   db,
+		tonClient:            tonClient,
+		tonMaxMainValidators: -1,
+		cache:                NewCache[string](),
 	}
 }
 
@@ -53,9 +66,11 @@ func (apiHandler JsonApiHandler) ServeHTTP(w http.ResponseWriter, r *http.Reques
 			payload, err = apiHandler.PlotTotalSupply()
 		case "plots_summary":
 			payload, err = apiHandler.GetPlotsSummary()
+		case "dkg_status":
+			payload, err = apiHandler.GetDkgStatus(r.Context())
 		default:
 			w.WriteHeader(http.StatusNotFound)
-			w.Write([]byte("Please select one of the next values: mints, burns, reinits, info, internal_keys, plot_minted, plot_burned, plot_total_supply, plots_summary"))
+			w.Write([]byte("Please select one of the next values: mints, burns, reinits, info, internal_keys, plot_minted, plot_burned, plot_total_supply, plots_summary, dkg_status"))
 			return
 		}
 
@@ -449,4 +464,185 @@ func (apiHandler JsonApiHandler) GetPlotsSummary() (string, error) {
 	}
 
 	return data, nil
+}
+
+func (apiHandler JsonApiHandler) GetDkgStatus(ctx context.Context) (string, error) {
+	type OriginalData struct {
+		Dkg     map[string]interface{}
+		PrevDkg map[string]interface{}
+	}
+
+	type DkgInfo struct {
+		VSetSize                int
+		ValidatorsCountInDkg    int
+		ValidatorsCountNotInDkg int
+		ValidatorsCountMax      int
+		ValidatorsCountTotal    int
+		ValidatorsIdxInDkg      map[int]string
+		ValidatorsIdxNotInDkg   map[int]string
+	}
+
+	type DkgStatus struct {
+		Dkg      DkgInfo
+		PrevDkg  DkgInfo
+		Original OriginalData
+	}
+
+	// Select DKG
+	dkg, err := apiHandler.SelectToObject("SELECT payload FROM metrics_data WHERE type_id = 0 ORDER BY id DESC LIMIT 1")
+	if err != nil {
+		return "", err
+	}
+
+	// Select prevDKG
+	prevDkg, err := apiHandler.SelectToObject("SELECT payload FROM metrics_data WHERE type_id = 1 ORDER BY id DESC LIMIT 1")
+	if err != nil {
+		return "", err
+	}
+
+	var status DkgStatus
+
+	// Fill DkgStatus
+	{
+		status.Original.Dkg = dkg
+		status.Original.PrevDkg = prevDkg
+
+		fillFn := func(dkg map[string]interface{}) (*DkgInfo, error) {
+			var dkgInfo DkgInfo
+
+			vset, ok := dkg["VSet"].(map[string]interface{})
+			if !ok {
+				return nil, errors.New("VSet has wrong type")
+			}
+
+			maxSignersJson, ok := dkg["MaxSigners"].(json.Number)
+			if !ok {
+				return nil, errors.New("MaxSigners has wrong type")
+			}
+
+			var dkgLastRound *map[string]interface{} = nil
+			roundNames := []string{"R3", "R2", "R1"}
+			for _, roundName := range roundNames {
+				if _, exists := dkg[roundName]; exists {
+					res, ok := dkg[roundName].(map[string]interface{})
+					if !ok {
+						return nil, fmt.Errorf("`%s` has wrong type", roundName)
+					}
+
+					dkgLastRound = &res
+					break
+				}
+			}
+
+			maxSigners, err := maxSignersJson.Int64()
+			if err != nil {
+				return nil, err
+			}
+
+			// tonMaxMainValidators
+			if apiHandler.tonMaxMainValidators < 0 {
+				block, err := apiHandler.tonClient.API.GetMasterchainInfo(ctx)
+				if err != nil {
+					return nil, fmt.Errorf("failed to get block: %v", err)
+				}
+
+				tonConfig, err := apiHandler.tonClient.API.GetBlockchainConfig(ctx, block, 16)
+				if err != nil {
+					return nil, fmt.Errorf("failed to get config: %v", err)
+				}
+
+				tonConfigParam16 := tonConfig.Get(16)
+				s := tonConfigParam16.BeginParse()
+				s.MustLoadUInt(16)
+				apiHandler.tonMaxMainValidators = int(s.MustLoadUInt(16))
+			}
+
+			dkgInfo.ValidatorsCountMax = apiHandler.tonMaxMainValidators
+			dkgInfo.VSetSize = len(vset)
+			dkgInfo.ValidatorsCountTotal = dkgInfo.ValidatorsCountMax
+			dkgInfo.ValidatorsCountInDkg = int(maxSigners)
+			if len(vset) < dkgInfo.ValidatorsCountTotal {
+				dkgInfo.ValidatorsCountTotal = len(vset)
+			}
+			dkgInfo.ValidatorsCountNotInDkg = dkgInfo.ValidatorsCountTotal - dkgInfo.ValidatorsCountInDkg
+
+			if dkgLastRound != nil {
+				maskJson, ok := (*dkgLastRound)["Mask"].(json.Number)
+				if !ok {
+					return nil, errors.New("mask has wrong type")
+				}
+
+				mask := new(big.Int)
+				mask, ok = mask.SetString(maskJson.String(), 10)
+				if !ok {
+					return nil, errors.New("invalid bigint")
+				}
+
+				dkgInfo.ValidatorsIdxInDkg = make(map[int]string)
+				dkgInfo.ValidatorsIdxNotInDkg = make(map[int]string)
+
+				for i := 0; i < dkgInfo.ValidatorsCountTotal; i++ {
+					pubKeyBase64, ok := vset[strconv.Itoa(i)].(string)
+					if !ok {
+						return nil, errors.New("invalid vset pubkey type")
+					}
+
+					if mask.Bit(i) == 1 {
+						dkgInfo.ValidatorsIdxInDkg[i] = pubKeyBase64
+					} else {
+						dkgInfo.ValidatorsIdxNotInDkg[i] = pubKeyBase64
+					}
+				}
+			}
+
+			return &dkgInfo, nil
+		}
+
+		statusDkg, err := fillFn(status.Original.Dkg)
+		if err != nil {
+			return "", err
+		}
+		status.Dkg = *statusDkg
+
+		statusPrevDkg, err := fillFn(status.Original.PrevDkg)
+		if err != nil {
+			return "", err
+		}
+		status.PrevDkg = *statusPrevDkg
+	}
+
+	jsonData, err := json.Marshal(status)
+	if err != nil {
+		return "", err
+	}
+
+	return string(jsonData), nil
+}
+
+func (apiHandler JsonApiHandler) SelectToObject(sql string) (map[string]interface{}, error) {
+	rows, err := apiHandler.db.Query(sql)
+	if err != nil {
+		return nil, err
+	}
+
+	var data string
+	if rows.Next() {
+		err = rows.Scan(&data)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if len(data) == 0 {
+		data = "{}"
+	}
+
+	jsonDec := json.NewDecoder(strings.NewReader(data))
+	jsonDec.UseNumber()
+
+	var m map[string]interface{}
+	if err := jsonDec.Decode(&m); err != nil {
+		return nil, err
+	}
+	return m, nil
 }
